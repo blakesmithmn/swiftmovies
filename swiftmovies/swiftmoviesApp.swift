f904d09ca2bf5c6ec7388a79b8b43eb3f26c30ff
Blake Smith <blake.smith.mn@gmail.com>
2023-03-23 12:43:48 -0500

    Initial Commit

---
+++ swiftmovies/swiftmoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftmoviesApp.swift
+//  swiftmovies
+//
+//  Created by Blake Smith on 3/23/23.
+//
+
+import SwiftUI
+
+@main
+struct swiftmoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
